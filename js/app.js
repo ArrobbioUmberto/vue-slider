807e2228841f3587c24155977d5e09371392ca56
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-20 17:37:56 +0100

    FEAT add methods for arrow

--- js/app.js
+++ js/app.js
@@ -3,37 +3,50 @@ console.log('ciao')
 const { createApp } = Vue
 
 createApp({
-        data() {
-            return {
-                slides:[
-                    {
-                        image: './img/01.webp',
-                        title: 'Marvel\'s Spiderman Miles Morale',
-                        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                    }, {
-                        image: './img/02.webp',
-                        title: 'Ratchet & Clank: Rift Apart',
-                        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                    }, {
-                        image: './img/03.webp',
-                        title: 'Fortnite',
-                        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                    }, {
-                        image: './img/04.webp',
-                        title: 'Stray',
-                        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                    }, {
-                        image: './img/05.webp',
-                        title: "Marvel's Avengers",
-                        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                    }
-                ]
+    data() {
+        return {
+            slides: [
+                {
+                    image: './img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: './img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: './img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: './img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: './img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ]
+        }
+    },
+    methods: {
+        previousImage() {
+            if (this.currentIndex === 0) {
+                this.currentIndex = this.slides.length - 1
+            } else {
+                this.currentIndex--
+            }
+        },
+        nextImage() {
+            if (this.currentIndex === this.slides.length - 1) {
+                this.currentIndex = 0
+            } else {
+                this.currentIndex++
             }
         }
-        // methods() {
-
-        // }
-    }).mount('.carosello')
+    }
+}).mount('.carosello')
 // per prima cosa generiamo i vari div che contengono le slide 
 // ADESSO CHE ABBIAMO GENERATO LE SLIDE DOBBIAMO COLLEGARE LE CLASSI ACTIVE E NON 
 
